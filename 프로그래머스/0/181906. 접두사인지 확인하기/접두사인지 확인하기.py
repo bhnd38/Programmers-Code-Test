0fc37c4252c190391effbb3e7704a026c2b52364
bhnd38 <157227970+bhnd38@users.noreply.github.com>
2024-05-28 17:36:53 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,10 +1,13 @@
 def solution(my_string, is_prefix):
-    a=''
-    if len(is_prefix) > len(my_string):
-        return 0
-    for c in range(0, len(is_prefix)): 
-        a+=my_string[c]
-    if a == is_prefix:
-        return 1
-    else:
-        return 0
+    
+    return 1 if my_string[:len(is_prefix)] == is_prefix else 0
+    
+    # a=''
+    # if len(is_prefix) > len(my_string):
+    #     return 0
+    # for c in range(0, len(is_prefix)): 
+    #     a+=my_string[c]
+    # if a == is_prefix:
+    #     return 1
+    # else:
+    #     return 0
