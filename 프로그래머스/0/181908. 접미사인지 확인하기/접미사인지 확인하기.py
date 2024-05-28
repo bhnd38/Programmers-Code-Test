9828a0333ef7affab967b605e5698806ba54fb10
bhnd38 <157227970+bhnd38@users.noreply.github.com>
2024-05-28 17:49:29 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,5 +1,5 @@
 def solution(my_string, is_suffix):
-    return 1 if my_string[len(my_string)-len(is_suffix):] == is_suffix else 0
+    return 1 if my_string[-len(is_suffix):] == is_suffix else 0
     
     
     # return int(my_string.endswith(is_suffix))
