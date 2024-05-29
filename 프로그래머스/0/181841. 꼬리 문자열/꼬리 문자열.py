b12a1d5c1a59ca5c5edae3c6b1d8c40b0d5ef0b5
bhnd38 <157227970+bhnd38@users.noreply.github.com>
2024-05-29 09:42:16 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
@@ -1,6 +1,9 @@
 def solution(str_list, ex):
     
     for x in str_list:
-        str_list = ['' if ex in x else x for x in str_list]
+        str_list = [x for x in str_list if ex not in x]
+        # 위의 식이 좀 더 간결하다.
+        
+        # str_list = ['' if ex in x else x for x in str_list]
             
     return ''.join(i for i in str_list)
